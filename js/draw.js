b5aeb1629830153f9ae89078204f341140ab6301
Maria Cristina Di Termine <mariacristinadt@gmail.com>
2015-03-10 15:45:57 +0100

    Improving of the code

--- js/draw.js
+++ js/draw.js
@@ -90,7 +90,8 @@ var drawModule = (function () {
         var snakeY = snake[i].y;
       
         if (food.x===snakeX && food.y === snakeY || food.y === snakeY && food.x===snakeX) {
-        food = new food;
+          food.x = Math.floor((Math.random() * 30) + 1);
+          food.y = Math.floor((Math.random() * 30) + 1);
         }
       }
   }
